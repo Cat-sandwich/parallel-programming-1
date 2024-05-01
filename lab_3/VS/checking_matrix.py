b9f4ed7cb1ea9b14073ac30b850f467c93d5fd60
Cat-sandwich <beni@ya.ru>
2024-05-01 10:05:58 +0400

    3 лабораторная работа с MPI

---
+++ lab_3/VS/checking_matrix.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def read(filename, matrix):
+    with open(filename) as file:
+        lines = [int(line.rstrip()) for line in file]
+    k = 0    
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            matrix[i][j] = int(lines[k])
+            k +=1
+    return matrix
+
+if __name__ == "__main__":
+    list_size = [ 50, 100, 500, 600, 700, 800, 900, 1000]
+    list_folder_name = ["E:\УЧЕБА\\parallel programming\\lab1\\lab_1\\VS\\data_matrix_1\\", "E:\\УЧЕБА\\parallel programming\\lab1\\lab_1\\VS\\data_matrix_2\\",
+                         "e:\УЧЕБА\\parallel programming\\lab1\\lab_2\\result_matrix\\"]
+    list_res_check = []
+
+    for i in list_size:
+        a =  np.empty([i, i], dtype=int)
+        a = read(list_folder_name[0] + str(i)  + ".txt",a)
+
+        b =  np.empty([i, i], dtype=int)
+        b = read(list_folder_name[1] + str(i) + ".txt", b)
+        res = np.empty([i, i], dtype=int)
+        res = np.dot(a, b)
+        c = np.empty([i, i], dtype=int)
+        c = read(list_folder_name[2] + str(i) + "_0"  + ".txt", c)
+        list_res_check.append(np.array_equal(res, c))
+
+    f1 = open("E:\УЧЕБА\\parallel programming\\lab1\\lab_2\\VS\\res_check.txt", "w+")
+    for i in range(len(list_res_check)):
+        if (i != len(list_res_check) - 1):
+            f1.write("Для матрицы размером "+ str(list_size[i]) + " x " + str(list_size[i]) +" результат проверки: " + str(list_res_check[i]) + "\n")
+        else:
+            f1.write("Для матрицы размером "+ str(list_size[i]) + " x " + str(list_size[i]) +" результат проверки: " + str(list_res_check[i]))
+    f1.close()
